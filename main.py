aaf2613eeac84db94e3dda9c15415947e459cee9
Lachlan Blake <lachlan.blake10@gmail.com>
2019-05-30 08:43:54 +1000

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ def handler(event, context):
         'certonly',                             # Obtain a cert but don't install it
         '-n',                                   # Run in non-interactive mode
         '--agree-tos',                          # Agree to the terms of service,
-        '--email', email,                       # Email
-        '--dns-cloudflare',                        # Use dns challenge with route53
-        '-d', domains,                          # Domains to provision certs for
+        '--email', 'contact@example.com',       # Email
+        '--dns-cloudflare',                     # Use dns challenge with route53
+        '-d', 'example.com',                    # Domains to provision certs for
         # Override directory paths so script doesn't have to be run as root
         '--config-dir', '/tmp/config-dir/',
         '--work-dir', '/tmp/work-dir/',
