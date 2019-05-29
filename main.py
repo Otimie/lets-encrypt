6f5bcf5e2d264cd4cd54d6c1e2495048cea11b58
Lachlan Blake <lachlan.blake10@gmail.com>
2019-05-30 08:42:42 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import certbot.main
 
-def handler():
+def handler(event, context):
+    certbot.main.main([
+        'certonly',                             # Obtain a cert but don't install it
+        '-n',                                   # Run in non-interactive mode
+        '--agree-tos',                          # Agree to the terms of service,
+        '--email', email,                       # Email
+        '--dns-cloudflare',                        # Use dns challenge with route53
+        '-d', domains,                          # Domains to provision certs for
+        # Override directory paths so script doesn't have to be run as root
+        '--config-dir', '/tmp/config-dir/',
+        '--work-dir', '/tmp/work-dir/',
+        '--logs-dir', '/tmp/logs-dir/',
+    ])
+    
     print(123)
 
-handler()
+handler('', '')
