b50bab45fc8b3011a3b4699e5dfdd67a7b67fc22
Lachlan Blake <lachlan.blake10@gmail.com>
2019-05-30 19:21:02 +1000

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def handler(event, context):
         '--agree-tos',                          # Agree to the terms of service,
         '--email', 'jhneibghbw@lachlanb.me',       # Email
         '--dns-cloudflare',                     # Use dns challenge with cloudflare
-        '--dns-cloudflare-credentials', 'cloudflare.ini'
+        '--dns-cloudflare-credentials', 'cloudflare.ini',
         '-d', 'lachlanb.me',                    # Domains to provision certs for
         # Override directory paths so script doesn't have to be run as root
         '--config-dir', '/tmp/config-dir/',
