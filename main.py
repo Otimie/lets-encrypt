a1be51430fcdf0e45426b3b796c5180728e309a0
Lachlan Blake <lachlan.blake10@gmail.com>
2019-05-30 19:18:17 +1000

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ def handler(event, context):
         '-n',                                   # Run in non-interactive mode
         '--agree-tos',                          # Agree to the terms of service,
         '--email', 'jhneibghbw@lachlanb.me',       # Email
-        '--dns-cloudflare',                     # Use dns challenge with route53
+        '--dns-cloudflare',                     # Use dns challenge with cloudflare
+        '--dns-cloudflare-credentials', 'cloudflare.ini'
         '-d', 'lachlanb.me',                    # Domains to provision certs for
         # Override directory paths so script doesn't have to be run as root
         '--config-dir', '/tmp/config-dir/',
